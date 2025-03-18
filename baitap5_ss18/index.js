c223975f345466d0f0cda7f1a1efc2699dc86b0c
Phat-Nguyen06 <kbiet20006@gmail.com>
2025-03-18 08:58:47 +0700

    Add files via upload

---
+++ baitap5_ss18/index.js
@@ -0,0 +1,14 @@
+let year = Number(prompt("Nhập số năm kinh nghiệm"));
+
+if (year < 1) {
+    alert("Mới vào nghề");
+}
+else if (year >= 1 && year <= 3) {
+    alert("Nhân viên có kinh nghiệm");
+}
+else if (year >= 4 && year <= 6) {
+    alert("Chuyên viên");
+}
+else {
+    alert("Quản lí");
+}
