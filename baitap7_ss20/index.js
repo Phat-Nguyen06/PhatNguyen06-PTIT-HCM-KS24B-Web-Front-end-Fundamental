583257a32038f3cf7e991c52147411919c6f016f
Phat-Nguyen06 <n24dtcn062@student.ptithcm.edu.vn>
2025-03-19 09:21:22 +0700

    Update index.js

--- baitap7_ss20/index.js
+++ baitap7_ss20/index.js
@@ -1,18 +1,22 @@
 let n = Number(prompt("Nhập số phần tử của dãy Fibonacci: "));
+if (isNaN(n) || n <= 0) {
+    console.log("Không hợp lệ! Vui lòng nhập một số nguyên dương.");
+}
+else {
+    let a = 1;
+    let b = 1;
+    let Fibonacci;
 
-let a = 1;
-let b = 1;
-let Fibonacci;
-
-for (let i = 0; i < n; i++) {
-    if (i == 0) {
-        Fibonacci = a;
-    } else if (i == 1) {
-        Fibonacci = b;
-    } else {
-        Fibonacci = a + b;
-        a = b;
-        b = Fibonacci;
+    for (let i = 0; i < n; i++) {
+        if (i == 0) {
+            Fibonacci = a;
+        } else if (i == 1) {
+            Fibonacci = b;
+        } else {
+            Fibonacci = a + b;
+            a = b;
+            b = Fibonacci;
+        }
+        console.log(Fibonacci);
     }
-    console.log(Fibonacci);
 }
