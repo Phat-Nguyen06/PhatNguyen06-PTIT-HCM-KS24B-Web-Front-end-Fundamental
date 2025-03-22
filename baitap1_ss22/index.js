dcbfd3c5b6b4c039069457a2a81cbe8a64548cc2
Phat-Nguyen06 <kbiet20006@gmail.com>
2025-03-22 16:56:47 +0700

    Add files via upload

---
+++ baitap1_ss22/index.js
@@ -0,0 +1,10 @@
+let number_array = [];
+console.log(number_array.length);
+
+number_array.push(1);
+number_array.push(2);
+number_array.push(3);
+number_array.push(4);
+number_array.push(5);
+
+console.log(number_array.length);
